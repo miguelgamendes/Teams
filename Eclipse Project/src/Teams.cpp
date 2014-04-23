43fe3029d014c4a1d28ad1dc5939633df3cbcb17
Johnstein <joao.neto.feup@gmail.com>
2014-04-23 15:29:13 +0100

    Created Eclipse Project
    Signed-off-by: Johnstein <joao.neto.feup@gmail.com>

---
+++ Eclipse Project/src/Teams.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Teams.cpp
+// Author      : AlgoNerds
+// Version     :
+// Copyright   : 
+// Description : Main File
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // prints Hello World!
+	return 0;
+}
